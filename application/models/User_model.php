e0acdf1c9cd9452709152b04a4dd4e90fe103dea
DESKTOP-FGRKD49\Asus <atinazr@gmail.com>
2018-05-21 23:37:35 +0700

    Login-Logout

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,7 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class User_model extends CI_Model{
-   public function register($enc_password){
+
+class User_model extends CI_Model {
+
+  public function register($enc_password){
        // Array data user
        $data = array(
            'nama' => $this->input->post('nama'),
@@ -14,5 +16,21 @@ class User_model extends CI_Model{
        // Insert user
        return $this->db->insert('users', $data);
    }
-  }
+
+    public function login($username, $password){
+       // Validasi
+       $this->db->where('username', $username);
+       $this->db->where('password', $password);
+
+       $result = $this->db->get('users');
+
+       if($result->num_rows() == 1){
+           return $result->row(0)->user_id;
+       } else {
+           return false;
+       }
+   }
+
+
+}
 
